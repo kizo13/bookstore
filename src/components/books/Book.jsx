2c5c18b9d725728dd40ccce03ba187d4f753377f
KiZo <kizo13@gmail.com>
2018-04-13 22:14:32 +0200

    change styling

--- src/components/books/Book.jsx
+++ src/components/books/Book.jsx
@@ -35,10 +35,11 @@ class Book extends React.Component {
                     <img className="book-cover" src={this.props.api.books.data.volumeInfo.imageLinks.thumbnail} />
                   </div>
                   <div className="column column-80">
-                    <h3 className="title">{this.props.api.books.data.volumeInfo.title} <small>({this.props.api.books.data.volumeInfo.publishedDate})</small></h3>
+                    <h3 className="title">{this.props.api.books.data.volumeInfo.title}</h3>
                     {this.props.api.books.data.volumeInfo.authors && (<div className="author">by <i>{this.props.api.books.data.volumeInfo.authors.join(', ')}</i></div>)}
-                    {this.props.api.books.data.volumeInfo.publisher && <span className="publisher">{this.props.api.books.data.volumeInfo.publisher}</span>}
-                    {this.props.api.books.data.volumeInfo.printedPageCount && <span className="pagecount">{this.props.api.books.data.volumeInfo.printedPageCount} pages</span>}
+                    <span className="published-date"><i class="fa fa-calendar" aria-hidden="true"></i> Published date: {this.props.api.books.data.volumeInfo.publishedDate}</span>
+                    {this.props.api.books.data.volumeInfo.publisher && <span className="publisher"> | <i class="fa fa-book" aria-hidden="true"></i> Published by: {this.props.api.books.data.volumeInfo.publisher}</span>}
+                    {this.props.api.books.data.volumeInfo.printedPageCount && <span className="pagecount"> | <i class="fa fa-file-text-o" aria-hidden="true"></i> {this.props.api.books.data.volumeInfo.printedPageCount} pages</span>}
                     {this.props.api.books.data.volumeInfo.description && <div className="description" dangerouslySetInnerHTML={{__html: this.props.api.books.data.volumeInfo.description}}></div>}
                     {!this.isBookAddedToCart && <button className="button button-outline" onClick={this.addToCart.bind(this, this.props.api.books.data)}><i className="fa fa-cart-plus" aria-hidden="true"></i> Add to cart</button>}
                     {this.isBookAddedToCart && <button className="button" onClick={this.removeFromCart.bind(this, this.props.api.books.data.id)}><i className="fa fa-shopping-cart" aria-hidden="true"></i> Remove from cart</button>}
--- src/components/search/SearchResultItem.jsx
+++ src/components/search/SearchResultItem.jsx
@@ -33,10 +33,13 @@ class SearchResultItem extends React.Component {
        */
       <li className="result-item">
         {/* <img src={this.props.book.volumeInfo.imageLinks.smallThumbnail} height="40" /> */}
-        <span className="title"><Link to={`/books/${this.props.book.id}`} className="link">{this.props.book.volumeInfo.title}</Link> <small>({this.props.book.volumeInfo.publishedDate})</small></span>
-        {this.props.book.volumeInfo.authors && (<span className="author">by <i>{this.props.book.volumeInfo.authors.join(', ')}</i></span>)}
-        {this.props.book.volumeInfo.publisher && <span className="publisher">{this.props.book.volumeInfo.publisher}</span>}
-        {this.props.book.volumeInfo.pageCount && <span className="pagecount">{this.props.book.volumeInfo.pageCount} pages</span>}
+        <span className="title">
+          <Link to={`/books/${this.props.book.id}`} className="link">{this.props.book.volumeInfo.title}</Link> 
+          {this.props.book.volumeInfo.authors && (<span className="author">by <i>{this.props.book.volumeInfo.authors.join(', ')}</i></span>)}
+        </span>
+        <span className="published-date"><i class="fa fa-calendar" aria-hidden="true"></i> Published date: {this.props.book.volumeInfo.publishedDate}</span>
+        {this.props.book.volumeInfo.publisher && <span className="publisher"> | <i class="fa fa-book" aria-hidden="true"></i> Published by: {this.props.book.volumeInfo.publisher}</span>}
+        {this.props.book.volumeInfo.pageCount && <span className="pagecount"> | <i class="fa fa-file-text-o" aria-hidden="true"></i> {this.props.book.volumeInfo.pageCount} pages</span>}
       </li>
     );
   }
