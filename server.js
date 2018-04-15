297926d79ab051ff6967fc30dd3fd504f3790421
KiZo <kizo13@gmail.com>
2018-04-15 10:14:06 +0200

    change server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 9001;
+const port = process.env.PORT || 80;
 
 app.get('/*', (req, res) => {
   res.sendFile(getFileForRequest(req.url));
