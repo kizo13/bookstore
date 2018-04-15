0c907020a48c75b4e30586c49d1a16904d1de3db
KiZo <kizo13@gmail.com>
2018-04-15 11:03:07 +0200

    small changes, entend README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# bookstore
+# Test bookstore app
+Live demo: https://lmi-test-bookstore.herokuapp.com/
+
+## Todo
+Changes needs to be done before app goes live:
+- introduce CI
+- set up multiple environments (e.g.: dev, qa, prod)
+- introduce environment variables
+- production javascript file needs to be minified and obfuscated
+- introduce typescript
+- add unit tests
+- add linter
+- change ui to responsive design
+- add pagination to search result page
+- add query debounce
--- src/components/books/Book.jsx
+++ src/components/books/Book.jsx
@@ -40,7 +40,7 @@ class Book extends React.Component {
                 </div>}
                 <div className={classNames('column', { 'column-80': this.props.api.books.data.volumeInfo.imageLinks })}>
                   <h3 className="title">{this.props.api.books.data.volumeInfo.title}</h3>
-                  <div className="author">by <i>{this.props.api.books.data.volumeInfo.authors.join(', ')}</i></div>
+                  {this.props.api.books.data.volumeInfo.authors && <div className="author">by <i>{this.props.api.books.data.volumeInfo.authors.join(', ')}</i></div>}
                   {this.props.api.books.data.volumeInfo.publishedDate && <span className="published-date"><i className="fa fa-calendar" aria-hidden="true"></i> <b>Published date:</b> {this.props.api.books.data.volumeInfo.publishedDate}</span>}
                   {this.props.api.books.data.volumeInfo.publisher && <span className="publisher"> | <i className="fa fa-book" aria-hidden="true"></i> <b>Published by:</b> {this.props.api.books.data.volumeInfo.publisher}</span>}
                   {this.props.api.books.data.volumeInfo.printedPageCount && <span className="pagecount"> | <i className="fa fa-file-text-o" aria-hidden="true"></i> {this.props.api.books.data.volumeInfo.printedPageCount} pages</span>}
